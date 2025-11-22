54f641d7a1dac2969f3424b7a6f302b121e4712c
Abdul Majeed Salim <majuprojects2025@gmail.com>
2025-11-22 16:02:42 +0530

    Time: 7 ms (82.42%) | Memory: 23 MB (19.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -11,13 +11,18 @@ class Solution:
         #     return False
 
         
-        clean = ''
-        for char in s:
-            if char.isalnum():
-                clean += char.lower()
+        # clean = ''
+        # for char in s:
+        #     if char.isalnum():
+        #         clean += char.lower()
         
-        rev = clean[::-1]
-        return clean == rev
+        # rev = clean[::-1]
+        # return clean == rev
+
+        
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        return s == s[::-1]
+
 
         
 
