2daa650175b873d2930e027352faab4da733ba8f
Abdul Majeed Salim <majuprojects2025@gmail.com>
2025-12-03 22:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (38.12%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,23 +1,41 @@
-def numNumber(n):
-        num = 0
-        while n :
-            digit = n%10
-            num = num + digit**2
-            n=n//10
-        return num
-class Solution:
+# def numNumber(n):
+#         num = 0
+#         while n :
+#             digit = n%10
+#             num = num + digit**2
+#             n=n//10
+#         return num
+# class Solution:
     
     
-    def isHappy(self, n: int) -> bool:
-        st = set()
-        while 1:
-            n = numNumber(n)
-            if n==1:
-                print(n)
-                return True
-            if n in st:
-                print(n)
-                return False
-            st.add(n)
+#     def isHappy(self, n: int) -> bool:
+#         st = set()
+#         while 1:
+#             n = numNumber(n)
+#             if n==1:
+#                 print(n)
+#                 return True
+#             if n in st:
+#                 print(n)
+#                 return False
+#             st.add(n)
 
+class Solution:
+    def getNext(self, n: int) -> int:
+        total = 0
+        while n:
+            n, d = divmod(n, 10)
+            total += d * d
+        return total
+
+    def isHappy(self, n: int) -> bool:
+        slow = n
+        fast = self.getNext(n)
+
+        # Floyd's cycle detection
+        while fast != 1 and slow != fast:
+            slow = self.getNext(slow)
+            fast = self.getNext(self.getNext(fast))
+
+        return fast == 1
     
