290c294358c3a59e011d78c2dd2ba21f05af5adf
Abdul Majeed Salim <majuprojects2025@gmail.com>
2025-12-06 00:22:56 +0530

    Time: 4 ms (79.54%) | Memory: 17.9 MB (39.02%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,10 +1,22 @@
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         reverse = 0
+#         temp = abs(x)
+#         if x< 0:
+#             return False
+#         while temp!=0:
+#             reverse = (reverse*10)+(temp%10)
+#             temp = temp//10
+#         return (reverse==abs(x))
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        reverse = 0
-        temp = abs(x)
-        if x< 0:
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-        while temp!=0:
-            reverse = (reverse*10)+(temp%10)
-            temp = temp//10
-        return (reverse==abs(x))
+
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        return x == reversed_half or x == reversed_half // 10
