2efad8a3a9eefb6a3ad87d650192342ca5beb89e
Abdul Majeed Salim <majuprojects2025@gmail.com>
2026-01-21 16:33:05 +0530

    Time: 452 ms (55.62%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,12 @@
-# Write your MySQL query statement below
-SELECT P2.id FROM Weather P1 
-JOIN Weather P2
-ON P2.recordDate = DATE_ADD(P1.recordDate, INTERVAL 1 DAY)
-WHERE P2.temperature>P1.temperature;
+-- # Write your MySQL query statement below
+-- SELECT P2.id FROM Weather P1 
+-- JOIN Weather P2
+-- ON P2.recordDate = DATE_ADD(P1.recordDate, INTERVAL 1 DAY)
+-- WHERE P2.temperature>P1.temperature;
+
+
+SELECT w2.id
+FROM Weather w2
+JOIN Weather w1
+ON w1.recordDate = DATE_SUB(w2.recordDate, INTERVAL 1 DAY)
+WHERE w2.temperature > w1.temperature;
