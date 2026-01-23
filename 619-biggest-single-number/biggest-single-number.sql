b3db931b53b032e65f6e6a498a266e199fad3ee0
Abdul Majeed Salim <majuprojects2025@gmail.com>
2026-01-23 17:01:36 +0530

    Time: 418 ms (41.38%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,6 +1,15 @@
-# Write your MySQL query statement below
-WITH uniqueNum AS 
-(SELECT num FROM MyNumbers
-GROUP BY 1
-HAVING COUNT(num)=1)
-SELECT MAX(num) as num FROM uniqueNum;
+-- # Write your MySQL query statement below
+-- WITH uniqueNum AS 
+-- (SELECT num FROM MyNumbers
+-- GROUP BY 1
+-- HAVING COUNT(num)=1)
+-- SELECT MAX(num) as num FROM uniqueNum;
+
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+) t;
+
