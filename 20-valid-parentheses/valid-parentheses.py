2288a2d3a145c6a4985751b470d61933126014f2
Abdul Majeed Salim <majuprojects2025@gmail.com>
2026-03-04 23:50:34 +0530

    Time: 4 ms (12.39%) | Memory: 19.3 MB (62.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,17 +1,14 @@
 class Solution:
-    
     def isValid(self, s: str) -> bool:
         stack = []
-        closeToOpen = {')': '(', '}': '{', ']': '['}
+        closeToOpen = {")":"(","]":"[","}":"{"}
 
-        for ch in s:
-            # If it's a closing bracket
-            if ch in closeToOpen:
-                if stack and stack[-1] == closeToOpen[ch]:
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
                     stack.pop()
                 else:
                     return False
             else:
-                stack.append(ch)
-
-        return len(stack) == 0
+                stack.append(c)
+        return True if not stack else False
