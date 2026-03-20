dbe5fd7263486c90bd2d1f67684186f43c116f24
Abdul Majeed Salim <majuprojects2025@gmail.com>
2026-03-20 11:33:39 +0530

    Time: 3 ms (22.67%) | Memory: 20.4 MB (49.43%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -12,27 +12,27 @@ class Solution:
         # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
         # Depth for search (iterative)
-        # res = 0
-        # stack = [[root,1]]
-        # while stack:
-        #     node, depth = stack.pop()
-        #     if node:
-        #         res = max(res, depth)
-        #         stack.append([node.left, depth+1])
-        #         stack.append([node.right, depth+1])
-        # return res
+        res = 0
+        stack = [[root,1]]
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth+1])
+        return res
         
         #Breadth for search 
-        level = 0
-        if not root:
-            return 0
-        q = deque([root])
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level+=1
-        return level    
+        # level = 0
+        # if not root:
+        #     return 0
+        # q = deque([root])
+        # while q:
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     level+=1
+        # return level    
