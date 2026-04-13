148469ea5729f4f0bf9370cf97090f0d17a34f68
Abdul Majeed Salim <majuprojects2025@gmail.com>
2026-04-13 19:14:42 +0530

    Time: 7 ms (47.21%) | Memory: 20.2 MB (79.49%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,21 +1,21 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # dp = [0] * (n+1)
-        # offset = 1
-        # for i in range(1,n+1):
-        #     if offset*2 == i:
-        #         offset = i
-        #     dp[i] = 1 + dp[i-offset]
-        # return dp
-        ad = []
-        for i in range(n+1):
-            ad.append(self.add(i))
-        return ad
+        dp = [0] * (n+1)
+        offset = 1
+        for i in range(1,n+1):
+            if offset*2 == i:
+                offset = i
+            dp[i] = 1 + dp[i-offset]
+        return dp
+    #     ad = []
+    #     for i in range(n+1):
+    #         ad.append(self.add(i))
+    #     return ad
 
-    def add(self, i):
-        res = 0 
-        while i:
-            if i%2 == 1:
-                res+=1
-            i = i//2
-        return res
+    # def add(self, i):
+    #     res = 0 
+    #     while i:
+    #         if i%2 == 1:
+    #             res+=1
+    #         i = i//2
+    #     return res
